2d1c178e65a632e9ef07df265b2aa4e40540e6d8
Tojo Rakotoarison <tojolalaina@gmail.com>
2022-06-02 10:26:12 +0200

    rails yelp mvp

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'simple_form', github: 'heartcombo/simple_form'
+# Gemfile
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+wrapfood = { name: 'Wrap Food', address: '7 ue, Paris E2 tyrE', phone_number: '537654645', category: 'french' }
+chinefood = { name: 'Food chine', address: '7 Boundary St, London E2 7JE', phone_number: '5348763', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: '56A Tanana High St, London E1 6PQ', phone_number: '5387653', category: 'italian' }
+dishoom = { name: 'Dishoom', address: '7 Bound St, London E2 7JE', phone_number: '5348763', category: 'chinese' }
+pasta_west = { name: 'Pasta West', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '538676763', category: 'italian' }
+goodfood = { name: 'Food fine', address: '7 Bound St, Paris E2 7JE', phone_number: '53765463', category: 'belgian' }
+
+[wrapfood, chinefood, pizza_east, dishoom, pasta_west, goodfood].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
